1da24995b12083dd0b54e4307eaaac2828d934b4
MZDDDD <74398419+MZDDDD@users.noreply.github.com>
2026-02-20 22:35:22 +0800

    Update AF_calculation.R

--- R/AF_calculation.R
+++ R/AF_calculation.R
@@ -1,85 +1,75 @@
-calc_af <- function(meta_res, params = list()) {
-#######
-library(magrittr)
-library(dplyr)
-library(lubridate) 
-library(gtools)
-library(stringr)
-library(tidyverse)
-library(lubridate)
-library(dlnm)
+calc_af <- function(dt_ew, dt_d, dt_ew_d, Date, lag_n, lag_max = 5, year_min = 2014) {
 
-lag_max <- 5 #VB-ZDs:24
+  library(dplyr)
+  library(stringr)
+  library(lubridate)
 
-dt_year<-data.frame(matrix(2014:2022,9,1))
-names(dt_year)<-"YEAR"
+  # 1) 生成 week-level 暴露（按 citycode-yearweek 汇总 p1）
+  # dt_ew 在你 data_reading 中已经有 p1_Pre（exposure count）
+  ew_week <- dt_ew %>%
+    mutate(date = as.Date(paste(Year, Month, Day, sep = "-"))) %>%
+    left_join(Date, by = "date") %>%   # Date 提供 yearweek
+    group_by(citycode, yearweek) %>%
+    summarise(p1 = sum(p1_Pre, na.rm = TRUE), .groups = "drop") %>%
+    arrange(citycode, yearweek) %>%
+    group_by(citycode) %>%
+    mutate(
+      lagN = 0
+    )
 
-dt_ep_ew_a <- dt_ew %>%
-  mutate(
-    date = as.Date(paste(Year,Month,Day,sep = "-"),origin = "1970-1-1")) %>% 
-  merge(.,Date,by.x = "date",by.y="date",all.x = TRUE) %>% 
-  arrange(citycode,Year,Month,Day)
+  # 过去 lag_max 周暴露累计（与你原来 lag5 类似）
+  for (k in 1:lag_max) {
+    ew_week <- ew_week %>% mutate(lagN = lagN + dplyr::lag(p1, k))
+  }
 
-dt_ew_ew_b <- dt_ew_ew_b %>%
-  arrange(citycode, yearweek) %>%
-  group_by(citycode) %>%
-  mutate(
-    lag5 = lag(p1,1) + lag(p1,2) + lag(p1,3) +
-      lag(p1,4) + lag(p1,5)
-  ) %>%
-  ungroup()
+  ew_week <- ew_week %>%
+    ungroup() %>%
+    mutate(
+      Year = as.integer(str_sub(yearweek, 1, 4)),
+      lag_flag = ifelse(lagN > 0, 1, 0),
+      X1 = paste0(citycode, "-", yearweek)
+    ) %>%
+    filter(Year >= year_min)
 
-dt_ew_ew_c<- dt_ew_ew_b %>% 
-  filter(Year>2013) %>% 
-  mutate(lag5 = ifelse(lag5>0,1,0)) %>% 
-  mutate(X1 = paste0(citycode,"-",yearweek))
+  lag_type <- ew_week %>% filter(lag_flag == 1) %>% pull(X1)
 
-# citycode-year-week
-city_yearweek_lag <- c(dt_ew_ew_c %>%
-                         ungroup() %>% 
-                         filter(lag5>0) %>%  #VB-ZDs:lag24
-                         select(X1)) 
+  # 2) 周病例（从 dt_ew_d 得到）
+  case_week <- dt_ew_d %>%
+    group_by(citycode, yearweek) %>%
+    summarise(case_wn = sum(incidence_n, na.rm = TRUE), .groups = "drop") %>%
+    mutate(
+      X1 = paste0(citycode, "-", yearweek),
+      YEAR = str_sub(yearweek, 1, 4)
+    )
 
-influ_num_d <- data.frame(matrix(NA, nrow = ncol(lag_n), ncol = 4))
-colnames(influ_num_d) <- c("lag","n_case","n_strata","n_obs")
+  case_week_exp <- case_week %>% filter(X1 %in% lag_type)
 
-lag_type <- city_yearweek_lag[[1]] 
-  
-dt_ew_d <- dt_ew_d%>%
-  mutate(week = as.numeric(str_sub(yearweek,6,7)))
-dt_influ_all1<- dt_d %>% 
-  mutate(YEAR = str_sub(yearweek,1,4)) %>% 
-  group_by(YEAR) %>% 
-  summarise(all_case = sum(incidence_n)) %>% 
-  ungroup()
-dt_influ_all<-merge(dt_year,dt_influ_all1,by.x = "YEAR",by.y = "YEAR" , all.x = TRUE) %>% 
-  mutate(all_case = ifelse(is.na(all_case),0,all_case))
-  
-dt_ew_d2<-dt_ew_d%>%
-  group_by(citycode,yearweek)%>%
-  summarise(
-    case_wn=sum(incidence_n)
-  )%>%
-  ungroup() %>% 
-  mutate(X1 = paste0(citycode,"-",yearweek)) %>% 
-  filter(X1 %in% lag_type)  
+  # 3) overall AF（你脚本的定义）
+  AF_overall <- sum(case_week_exp$case_wn, na.rm = TRUE) / sum(dt_ew_d$incidence_n, na.rm = TRUE)
 
-AF <- sum(dt_ew_d2$case_wn) /sum(dt_ew_d$incidence_n)
- 
-#year AF  
-dt_influ_all2<- dt_ew_d2 %>% 
-   mutate(YEAR = str_sub(yearweek,1,4)) %>% 
-   group_by(YEAR) %>% 
-   summarise(all_influ_case = sum(case_wn)) %>% 
-   ungroup() %>% 
-   arrange(YEAR) 
- 
+  # 4) year AF
+  all_case_year <- dt_ew_d %>%
+    mutate(YEAR = str_sub(yearweek, 1, 4)) %>%
+    group_by(YEAR) %>%
+    summarise(all_case = sum(incidence_n, na.rm = TRUE), .groups = "drop")
 
-dt_all_inf<-dt_all_inf1 %>% 
-  mutate(all_case =ifelse(is.na(all_case),0,all_case),
-          all_influ_case = ifelse(is.na(all_influ_case),0,all_influ_case)) %>%
-  mutate(AF = all_influ_case/all_case)
+  exp_case_year <- case_week_exp %>%
+    group_by(YEAR) %>%
+    summarise(all_influ_case = sum(case_wn, na.rm = TRUE), .groups = "drop")
 
-return(af_res)
+  af_year <- all_case_year %>%
+    left_join(exp_case_year, by = "YEAR") %>%
+    mutate(
+      all_influ_case = ifelse(is.na(all_influ_case), 0, all_influ_case),
+      AF = all_influ_case / all_case
+    ) %>%
+    arrange(YEAR)
+
+  # 5) 输出（明确创建 af_res 再 return）
+  af_res <- list(
+    AF_overall = AF_overall,
+    AF_by_year = af_year
+  )
+
+  return(af_res)
 }
-
